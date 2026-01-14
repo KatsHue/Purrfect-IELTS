15766adbb6f3f493d6232a3cb3dec72a0e483b28
KatsHue <katia.shuerta@gmail.com>
2026-01-14 17:56:07 -0600

    Link de chatbot agregado

--- Frontend_Proyecto/src/api/ChatBotAPI.ts
+++ Frontend_Proyecto/src/api/ChatBotAPI.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:5000/chat";
+const API_URL = import.meta.env.VITE_CHATBOT_API;
 
 export const sendMessageToChatbot = async (
   message: string
