878feffd2c22a997b06879a6b6e8b1210fac4294
KatsHue <katia.shuerta@gmail.com>
2026-05-01 13:41:19 -0600

    Tiempo de carga reducido

--- Frontend_Proyecto/src/hooks/useAuth.ts
+++ Frontend_Proyecto/src/hooks/useAuth.ts
@@ -1,30 +1,30 @@
 import { getUser } from "@/api/AuthAPI";
 import { useQuery } from "@tanstack/react-query";
-import { useEffect, useState} from "react";
+import { useEffect, useState } from "react";
 
 export const useAuth = () => {
-    const {data, isError, isLoading} = useQuery({
-        queryKey: ['user'],
-        queryFn: getUser,
-        retry: 1,
-        refetchOnWindowFocus: false
-    })
+  const { data, isError, isLoading } = useQuery({
+    queryKey: ["user"],
+    queryFn: getUser,
+    retry: 1,
+    refetchOnWindowFocus: false,
+  });
 
-    const [minLoading, setMinLoading] = useState(true);
+  const [minLoading, setMinLoading] = useState(true);
 
-    useEffect(() => {
-        if (!isLoading) {
-            const timer = setTimeout(() => {
-                setMinLoading(false);
-            }, 3000); 
+  useEffect(() => {
+    if (!isLoading) {
+      const timer = setTimeout(() => {
+        setMinLoading(false);
+      }, 1500);
 
-            return () => clearTimeout(timer);
-        }
-    }, [isLoading]);
+      return () => clearTimeout(timer);
+    }
+  }, [isLoading]);
 
-    // Si hay error, no bloquear por falta de data
-    const loading = isLoading || minLoading || (!data && !isError);
-   // const loading = isLoading || minLoading || !data;
+  // Si hay error, no bloquear por falta de data
+  const loading = isLoading || minLoading || (!data && !isError);
+  // const loading = isLoading || minLoading || !data;
 
-    return { data, isError, isLoading : loading };
-}
+  return { data, isError, isLoading: loading };
+};
